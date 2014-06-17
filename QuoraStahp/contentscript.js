bea47dc0c25c3fbbab95872b720a9592daa62e16
Shaju Mohammed <shajumohamed@yahoo.com>
2014-06-18 00:37:04 +0530

    Adding Source

---
+++ QuoraStahp/contentscript.js
@@ -0,0 +1,21 @@
+
+if(document.getElementsByClassName('modal_signup_background')[0]!=null)
+{
+	if(document.getElementsByClassName('modal_signup_background')[0].parentNode!=null)
+	{
+		document.getElementsByClassName('modal_signup_background')[0].parentNode.style.display="none";
+		console.log("modal sign up hidden");
+	}
+}
+
+if(document.getElementsByClassName('blurred_answer with_signup')[0]!=null)
+{
+	document.getElementsByClassName('blurred_answer with_signup')[0].style.display="none";
+	console.log("answer blurring removed");
+
+}
+
+
+
+
+
